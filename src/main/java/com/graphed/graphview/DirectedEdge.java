52467281ab975a1386db390b372b461dc893c016
unknown <diaslucas8822@gmail.com>
2024-02-10 10:57:03 -0300

    fixed the directed edge indicator Closes #2

--- src/main/java/com/graphed/graphview/DirectedEdge.java
+++ src/main/java/com/graphed/graphview/DirectedEdge.java
@@ -21,6 +21,10 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class DirectedEdge extends Edge {
+    private static final double ARROW_ANGLE = 20;
+    private static final double ARROW_LENGTH = 10;
+    private double intersectionX, intersectionY;
+
     public DirectedEdge(Vertex v1, Vertex v2) {
         super(v1, v2);
     }
@@ -44,23 +48,50 @@ public class DirectedEdge extends Edge {
         gc.setLineWidth(3);
         gc.strokeLine(v1.posX, v1.posY, v2.posX, v2.posY);
 
-        // calculates the intersection point of the edge and the border of the vertex
+        drawArrow(gc);
+    }
+
+    public void drawArrow(GraphicsContext gc) {
+        double angle = calculateAngle();
+        calculateIntersection();
+
+        drawArrowLine(gc, angle - ARROW_ANGLE);
+        drawArrowLine(gc, angle + ARROW_ANGLE);
+    }
+
+    private void drawArrowLine(GraphicsContext gc, double angle) {
+        double opposite = Math.sin(Math.toRadians(angle)) * ARROW_LENGTH;
+        double adjacent = Math.cos(Math.toRadians(angle)) * ARROW_LENGTH;
+
+        double x = intersectionX + adjacent;
+        double y = intersectionY - opposite;
+
+        gc.setStroke(Color.GREEN);
+        gc.setLineWidth(3);
+        gc.strokeLine(intersectionX, intersectionY, x, y);
+    }
+
+    // Calculates the angle of the edge formed by two vertices (v1 and v2) in
+    // relation to the x-axis.
+    // Returns the angle in degrees.
+    public double calculateAngle() {
+        double angle = Math.toDegrees(Math.atan2(v2.posY - v1.posY, v1.posX - v2.posX));
+        if (angle < 0) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    // calculates the intersection point of the edge and the border of the vertex
+    public void calculateIntersection() {
         double X = v2.posX - v1.posX;
         double Y = v2.posY - v1.posY;
         double XY = Math.pow(X, 2) + Math.pow(Y, 2);
         double H = Math.sqrt(XY);
         double x = Vertex.radius * X / H;
         double y = Vertex.radius * Y / H;
-        double intersectionX, intersectionY;
         intersectionX = v2.posX - x;
         intersectionY = v2.posY - y;
-
-        // draws a green circle to indicate the direction of the edge
-        gc.setFill(Color.GREEN);
-        gc.beginPath();
-        gc.setLineWidth(2.5);
-        gc.arc(intersectionX, intersectionY, 7, 7, 0, 360);
-        gc.closePath();
-        gc.fill();
     }
+
 }
